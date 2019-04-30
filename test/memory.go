b2db45d09e05df9526c162de81af2ee2870faa2f
electricface <songwentai@deepin.com>
2019-04-30 10:31:16 +0800

    fix: memory test failed on arm64 arch

--- test/memory.go
+++ test/memory.go
@@ -1,24 +1,39 @@
 package main
 
-import "pkg.deepin.io/gir/gio-2.0"
+import (
+	"pkg.deepin.io/gir/gio-2.0"
+	"runtime"
+	"strings"
+)
 import "time"
-import "runtime"
 import "syscall"
 import "fmt"
 import "os"
 
 func RSSinMB() int {
 	var r syscall.Rusage
-	syscall.Getrusage(syscall.RUSAGE_SELF, &r)
+	err := syscall.Getrusage(syscall.RUSAGE_SELF, &r)
+	if err != nil {
+		panic(err)
+	}
 	return int(r.Maxrss) / 1024
 }
 
 func TestFunc() {
 	gio.AppInfoGetAll()
 	gio.DesktopAppInfoSearch("d")
+	runtime.GC()
 }
 
 func main() {
+	var limit = 30
+	if strings.HasPrefix(runtime.GOARCH, "arm") {
+		// GOARCH is arm or arm64
+		limit = 200
+	}
+	fmt.Printf("limit: %dMB\n", limit)
+
+	TestFunc()
 	baseRSSinMB := RSSinMB()
 	fmt.Printf("base: %dMB\n", baseRSSinMB)
 	getIncreased := func() int {
@@ -31,12 +46,10 @@ func main() {
 	})
 	for {
 		TestFunc()
-		const limit = 30
 		increasedInMB := getIncreased()
 		if increasedInMB > limit {
 			fmt.Printf("increased: %dMB\n", increasedInMB)
 			panic(fmt.Errorf("increased beyond %dMB, detect a memory leak", limit))
 		}
-		runtime.GC()
 	}
 }
