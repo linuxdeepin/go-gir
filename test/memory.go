de3669459f053f56896ea3cb2bad1eced4faa59e
weizhixiang <weizhixiang@uniontech.com>
2021-12-05 20:04:05 +0800

    fix: 添加必要注释
    添加必要注释
    
    Log:
    Task: https://pms.uniontech.com/zentao/task-view-79863.html
    Influence: 无
    Change-Id: I548c642a920a1b6be96feec200689e2d752c8dce

--- test/memory.go
+++ test/memory.go
@@ -28,7 +28,7 @@ func TestFunc() {
 
 func main() {
 	var limit = 60
-	if strings.HasPrefix(runtime.GOARCH, "arm") {
+	if strings.HasPrefix(runtime.GOARCH, "arm") {	// 易阻塞CRP打包，只在本地进行测试
 		// GOARCH is arm or arm64
 		limit = 200
 	}
